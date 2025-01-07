9ad792566c758ee1731dc73aab4cdd1fdd0f655e
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-07 17:33:10 +0100

    feat: second milestone completed

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,15 +1,26 @@
+//Importo l'array dei post:
+const posts = require('../data/postsArray');
+
+
 //Metodi delle rotte CRUD:
 /*
 Cioè funzioni che, una volta importate nel file del router,
 svolgeranno le funzioni di ogni determinata rotta
 */
-
 const index = (req, res) => {
-   res.send('Lista dei post');
+   //Alla richiesta di visualizzare i post, restituisco tutta la lista dei post in formato json:
+   res.json(posts);
 };
 
 const show = (req, res) =>{
-   res.send(`Visualizza post con id: ${req.params.id}`);
+   //Salvo il parametro che mi "arriva" dalla richiesta:
+   const id = req.params.id;
+
+   //Creo la logica per trovare il post, con quel determinato parametro richiesto (in questo caso l'id):
+   let post = posts.find(post => post.id == id);
+
+   //La risposta restituirà il post in formato json:
+   res.json(post);
 };
 
 const store = (req, res) =>{
@@ -25,7 +36,20 @@ const modify = (req, res) =>{
 };
 
 const destroy = (req,res) =>{
-   res.send(`Ellimino il post con id: ${req.params.id}`);
+   //Salvo il parametro che mi "arriva" dalla richiesta:
+   const id = req.params.id;
+
+   //Creo la logica per trovare il post, con quel determinato parametro richiesto (in questo caso l'id):
+   let post = posts.find(post => post.id == id);
+
+   //Logica per eliminare il post richiesto con splice: (a splice passo come indice di partenza il post trovato con find)
+   posts.splice(posts.indexOf(post), 1);
+
+   //faccio un console log della lista dei post, per vederla nel terminale aggiornata:
+   console.log(posts)
+
+   //Rispondo con uno status 204 nel caso l'elliminazione abbia avuto successo:
+   res.sendStatus(204)
 };
 
 //Esporto il modulo delle funzioni che assegneremo alle rotte:
@@ -37,3 +61,5 @@ module.exports = {
    modify,
    destroy
 }
+
+
